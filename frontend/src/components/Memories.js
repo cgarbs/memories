ceb6cb6302c1632eaa60c7a61ada32d2951be79c
Christian <christian.garba@gmail.com>
2021-05-26 23:57:55 -0700

    begin creating main pages

---
+++ frontend/src/components/Memories.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Memories extends Component {
+    render() {
+        return (
+            <div>
+                Memories will go here!
+            </div>
+        );
+    }
+}
+
+export default Memories;
